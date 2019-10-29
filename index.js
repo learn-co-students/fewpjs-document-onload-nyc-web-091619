38e2edbfe92e6a8890b466958a22319f68138340
Michelle Lee <mlee092@Michelles-MacBook.local>
2019-10-28 22:23:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+
+// add the .addEventListener
+
+document.addEventListener( "DOMContentLoaded", function () {
+    updateDOM()
+} );
+
+// ✓ should change the text on the page when DOM content has loaded
+  function updateDOM() {
+    document.getElementById( "text" )
+      .innerHTML = "This is really cool!";
+  }
