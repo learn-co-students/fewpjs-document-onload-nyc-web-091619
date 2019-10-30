41ea28814858b1b1ef0ee194c256782a396ea902
Jonathan Tan <you@example.com>
2019-10-29 20:36:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Your code goes here
+// document.addEventListener("DomContentLoaded", function(e){
+//     const p = document.getElementById("text")
+//     p.textContent = "This is really cool!"
+// });
+
+
+
+document.addEventListener( "DOMContentLoaded", function () {
+    document.getElementById( "text" ).innerHTML = "This is really cool!";
+  } );
+  
